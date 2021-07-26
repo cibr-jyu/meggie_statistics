3022de7e4ef5233aaa6e6a068604aee0d4b7c993
Erkka Heinila <erkkahe@gmail.com>
2021-07-26 12:34:35 +0300

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='meggie_statistics',
+    version='0.1.0',
+    license='BSD',
+    packages=['meggie_statistics'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'meggie>=1.2.0'
+    ]
+)

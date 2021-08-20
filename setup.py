c2be52c765dc47ac6232b9bf91cbb5fbf1ee31e9
Erkka Heinila <erkkahe@gmail.com>
2021-08-20 18:30:28 +0300

    Release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='meggie_statistics',
-    version='0.2.0',
+    version='0.2.1',
     description='Statistics plugin for Meggie',
     license='BSD',
     packages=['meggie_statistics'],
